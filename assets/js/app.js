dc1031f877bddc7988f920c61df09955935637c4
VaflyaArtek <yuriy.shpak.2017@gmail.com>
2023-11-29 01:25:59 +0200

    without adaptive

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+// swiper
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
