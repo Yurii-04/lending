1a36941b0e0be6373a841d412908a2176689cf19
VaflyaArtek <yuriy.shpak.2017@gmail.com>
2023-11-29 18:44:28 +0200

    completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,24 @@
 // swiper
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
     loop: true,
 
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  
+
     // And if we need scrollbar
     scrollbar: {
-      el: '.swiper-scrollbar',
+        el: ".swiper-scrollbar",
     },
-  });
+});
+
+// hamburger
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-nav");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("is-active");
+    mobileMenu.classList.toggle("is-active");
+});
